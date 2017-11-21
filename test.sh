48cea51ce0cd9dcf120ebc49befdcec0f73dc5c3
vkiet <opendoo4@gmail.com>
2017-11-21 17:11:09 +0700

    add test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+../wrk2/wrk -t2 -c3 -d3s -R2000 -s ./card.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c3 -d3s -R2000 -s ./district.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c3 -d3s -R2000 -s ./getmember.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c3 -d3s -R2000 -s ./nationality.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c3 -d3s -R2000 -s ./province.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c3 -d3s -R2000 -s ./proxyIdCard.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c3 -d3s -R2000 -s ./subdistrict.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c3 -d3s -R2000 -s ./title.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
