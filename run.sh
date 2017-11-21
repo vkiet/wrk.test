46017acdc17c0357fd1676c4c6914eb0829f2679
vkiet <opendoo4@gmail.com>
2017-11-21 17:00:38 +0700

    add script

---
+++ run.sh
@@ -0,0 +1,8 @@
+../wrk2/wrk -t2 -c10000 -d60s -R2000 -s ./card.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c10000 -d60s -R2000 -s ./district.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c10000 -d60s -R2000 -s ./getmember.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c10000 -d60s -R2000 -s ./nationality.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c10000 -d60s -R2000 -s ./province.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c10000 -d60s -R2000 -s ./proxyIdCard.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c10000 -d60s -R2000 -s ./subdistrict.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
+#../wrk2/wrk -t2 -c10000 -d60s -R2000 -s ./title.lau https://cloudservices.kingpower.com/production/v5/MEMBER/MemberService.svc
